5db95741939281df4256e9f3358a81551b4dffd9
nathandrapeza <drapeza.nathan@gmail.com>
2024-12-28 15:58:53 -0700

    Added image capture functionality to keyboard control.

--- image_capture.py
+++ image_capture.py
@@ -1,14 +1,70 @@
 from djitellopy import tello
+import time
+import key_press_module
 import cv2
+import os
 
-drone = tello.Tello()
-drone.connect()
-print(drone.get_battery())
 
-drone.streamon()
+def get_keyboard_input():
+    """
+    Gets the user's keyboard input, and returns a list of values to be used to control the drone.
 
-while True:
-    img = drone.get_frame_read().frame
-    img = cv2.resize(img, (360, 240))
-    cv2.imshow("Stream", img)
-    cv2.waitKey(1) # 1ms delay
+    drone (tello.Tello): Tello object representing the physical drone being used.
+
+    Returns a list of values to be used as arguments to command the drone's direction/movement.
+    """
+    left_right, forward_back, up_down, yaw = 0, 0, 0, 0
+    speed = 50
+
+    if key_press_module.get_key("LEFT"):
+        left_right = -speed
+    elif key_press_module.get_key("RIGHT"):
+        left_right = speed
+    elif key_press_module.get_key("UP"):
+        forward_back = speed
+    elif key_press_module.get_key("DOWN"):
+        forward_back = -speed
+    elif key_press_module.get_key("w"):
+        up_down = speed
+    elif key_press_module.get_key("s"):
+        up_down = -speed
+    elif key_press_module.get_key("a"):
+        yaw = speed
+    elif key_press_module.get_key("d"):
+        yaw = -speed
+    elif key_press_module.get_key("e"):
+        drone.takeoff()
+    elif key_press_module.get_key("q"):
+        drone.land()
+    elif key_press_module.get_key("z"):
+        cv2.imwrite(f"resources/images/{time.time()}.jpg", img)
+        time.sleep(0.3)
+
+    return [left_right, forward_back, up_down, yaw]
+
+
+def main():
+    global img
+
+    global drone
+    os.makedirs("resources/images", exist_ok=True)
+    key_press_module.init()
+
+    drone = tello.Tello()
+    drone.connect()
+    print(drone.get_battery())
+
+    drone.streamon()
+
+    while True:
+        left_right, forward_back, up_down, yaw = get_keyboard_input()
+        drone.send_rc_control(left_right, forward_back, up_down, yaw)
+
+        img = drone.get_frame_read().frame
+        img = cv2.resize(img, (144, 144))
+        cv2.imshow("Stream", img)
+        cv2.waitKey(1) # 1ms delay
+
+
+if __name__ == '__main__':
+    main()
--- keyboard_control.py
+++ keyboard_control.py
@@ -1,47 +1,55 @@
 from djitellopy import tello
 from time import sleep
-import KeyPressModule
-
-KeyPressModule.init()
-
-drone = tello.Tello()
-drone.connect()
-print(drone.get_battery())
+import key_press_module
 
 def get_keyboard_input():
+    """
+    Gets the user's keyboard input, and returns a list of values to be used to control the drone.
+
+    drone (tello.Tello): Tello object representing the physical drone being used.
+
+    Returns a list of values to be used as arguments to command the drone's direction/movement.
+    """
     left_right, forward_back, up_down, yaw = 0, 0, 0, 0
     speed = 50
 
-    if KeyPressModule.get_key("LEFT"):
+    if key_press_module.get_key("LEFT"):
         left_right = -speed
-    elif KeyPressModule.get_key("RIGHT"):
+    elif key_press_module.get_key("RIGHT"):
         left_right = speed
-    elif KeyPressModule.get_key("UP"):
+    elif key_press_module.get_key("UP"):
         forward_back = speed
-    elif KeyPressModule.get_key("DOWN"):
+    elif key_press_module.get_key("DOWN"):
         forward_back = -speed
-    elif KeyPressModule.get_key("w"):
+    elif key_press_module.get_key("w"):
         up_down = speed
-    elif KeyPressModule.get_key("s"):
+    elif key_press_module.get_key("s"):
         up_down = -speed
-    elif KeyPressModule.get_key("a"):
+    elif key_press_module.get_key("a"):
         yaw = speed
-    elif KeyPressModule.get_key("d"):
+    elif key_press_module.get_key("d"):
         yaw = -speed
-    elif KeyPressModule.get_key("e"):
+    elif key_press_module.get_key("e"):
         drone.takeoff()
-    elif KeyPressModule.get_key("q"):
+    elif key_press_module.get_key("q"):
         drone.land()
 
     return [left_right, forward_back, up_down, yaw]
 
 
 def main():
+    global drone
+
+    key_press_module.init()
+
+    drone = tello.Tello()
+    drone.connect()
+    print(drone.get_battery())
+
     while True:
         left_right, forward_back, up_down, yaw = get_keyboard_input()
         drone.send_rc_control(left_right, forward_back, up_down, yaw)
         sleep(0.05)
-
 
 if __name__ == '__main__':
     main()
